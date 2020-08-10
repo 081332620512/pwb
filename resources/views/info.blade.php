675ada24c28bbd87a4f3c7a8b625edc20a95a1fc
081332620512 <febrimozak@gmail.com>
2020-08-10 23:32:15 +0700

    project

---
+++ resources/views/info.blade.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>About ZOE</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="/css/fontAwesome.css">
+        <link rel="stylesheet" href="/css/style.css">
+
+    </head>
+    <body>
+        
+
+        <section class="first-gallery-section">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="text-content">
+                  <h2>About ZOE</h2>
+                  <div class="line-dec"></div>
+                  <span>All about our work and play</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section class="third-section">
+            <div class="container">
+                <div class="col-md-10 col-md-offset-1">
+                  <div class="">
+                    <img src="/img/right-image.png" alt="">
+                  </div>
+                  <div class="">
+                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, atque.</h4>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit est earum eum eaque, exercitationem doloribus corrupti iusto esse perspiciatis omnis. Minus, aspernatur facere minima neque explicabo nemo distinctio quod optio quibusdam ipsam in ex nam odio, nesciunt eum aliquam sint dolores. Omnis sed nihil ipsum mollitia? Maiores esse magni, temporibus atque doloremque asperiores ex eum incidunt porro fugiat architecto illo quidem dolorum nihil non numquam suscipit. Dicta numquam inventore earum, quidem culpa, tenetur mollitia, omnis tempora odio excepturi soluta ipsam sed voluptatem. Architecto rerum maiores, nulla nemo quae praesentium eos. Ad magni voluptates maiores sapiente, blanditiis odio culpa repudiandae at!</p>
+                  </div>
+                </div>
+            </div>
+          </section>
+
+        <footer>
+          <div class="container">
+            <div class="row">
+              <div class="col-md-12"></div>
+                <p>This is Footer !!!</p>
+              </div>
+            </div>
+          </div>
+        </footer>
+    </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,162 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>ZOE Company</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="/css/fontAwesome.css">
+        <link rel="stylesheet" href="/css/style.css">
+
+    </head>
+    <body>
+
+        <section class="first-section">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="text-content">
+                  <h2>Welcome To ZOE</h2>
+                  <div class="line-dec"></div>
+                  <span>This is clean professional asurance company</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section class="second-section">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-3 col-sm-6">
+                <div class="service-item">
+                  <div class="icon">
+                    <img src="/img/second-icon.png" alt="">
+                  </div>
+                  <h4>Lorem, ipsum dolor.</h4>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa reprehenderit consequatur hic quasi porro assumenda cum ab unde quis. Consequuntur.</p>
+                </div>
+              </div>
+              <div class="col-md-3 col-sm-6">
+                <div class="service-item">
+                  <div class="icon">
+                    <img src="/img/second-icon.png" alt="">
+                  </div>
+                  <h4>Lorem, ipsum dolor.</h4>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa reprehenderit consequatur hic quasi porro assumenda cum ab unde quis. Consequuntur.</p>
+                </div>
+              </div>
+              <div class="col-md-3 col-sm-6">
+                <div class="service-item">
+                  <div class="icon">
+                    <img src="/img/second-icon.png" alt="">
+                  </div>
+                  <h4>Lorem, ipsum dolor.</h4>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa reprehenderit consequatur hic quasi porro assumenda cum ab unde quis. Consequuntur.</p>
+                </div>
+              </div>
+              <div class="col-md-3 col-sm-6">
+                <div class="service-item">
+                  <div class="icon">
+                    <img src="/img/second-icon.png" alt="">
+                  </div>
+                  <h4>Lorem, ipsum dolor.</h4>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa reprehenderit consequatur hic quasi porro assumenda cum ab unde quis. Consequuntur.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section class="third-section">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-10 col-md-offset-1">
+                <div class="left-image col-md-4">
+                  <img src="/img/right-image.png" alt="">
+                </div>
+                <div class="right-text col-md-8">
+                  <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, atque.</h4>
+                  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit est earum eum eaque, exercitationem doloribus corrupti iusto esse perspiciatis omnis. Minus, aspernatur facere minima neque explicabo nemo distinctio quod optio quibusdam ipsam in ex nam odio, nesciunt eum aliquam sint dolores. Omnis sed nihil ipsum mollitia? Maiores esse magni, temporibus atque doloremque asperiores ex eum incidunt porro fugiat architecto illo quidem dolorum nihil non numquam suscipit. Dicta numquam inventore earum, quidem culpa, tenetur mollitia, omnis tempora odio excepturi soluta ipsam sed voluptatem. Architecto rerum maiores, nulla nemo quae praesentium eos. Ad magni voluptates maiores sapiente, blanditiis odio culpa repudiandae at!</p>
+                  <a href="/info" class="primary-button">Visit More</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section class="fourth-section">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-3 col-sm-6">
+                <div class="portfolio-item third-item">
+                  <div class="image">
+                    <img src="/img/third-item.jpg"></a>
+                  </div>
+                  <div class="text">
+                    <h4>Lorem, ipsum dolor.</h4>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-3 col-sm-6">
+                <div class="portfolio-item third-item">
+                  <div class="image">
+                    <img src="/img/third-item.jpg"></a>
+                  </div>
+                  <div class="text">
+                    <h4>Lorem, ipsum dolor.</h4>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-3 col-sm-6">
+                <div class="portfolio-item third-item">
+                  <div class="image">
+                    <img src="/img/third-item.jpg"></a>
+                  </div>
+                  <div class="text">
+                    <h4>Lorem, ipsum dolor.</h4>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-3 col-sm-6">
+                <div class="send-to-portfolio">
+                  <span>Do you want to see more?</span>
+                  <div class="primary-button">
+                    <a href="/galery">Visit gallery</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section class="fivth-section">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-10 col-md-offset-1"> 
+                <div class="left-text col-md-8">
+                  <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, atque.</h4>
+                  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit est earum eum eaque, exercitationem doloribus corrupti iusto esse perspiciatis omnis. Minus, aspernatur facere minima neque explicabo nemo distinctio quod optio quibusdam ipsam in ex nam odio, nesciunt eum aliquam sint dolores. Omnis sed nihil ipsum mollitia? Maiores esse magni, temporibus atque doloremque asperiores ex eum incidunt porro fugiat architecto illo quidem dolorum nihil non numquam suscipit. Dicta numquam inventore earum, quidem culpa, tenetur mollitia, omnis tempora odio excepturi soluta ipsam sed voluptatem. Architecto rerum maiores, nulla nemo quae praesentium eos. Ad magni voluptates maiores sapiente, blanditiis odio culpa repudiandae at!</p>
+                </div>
+                <div class="right-image col-md-4">
+                  <img src="/img/right-image.png" alt="">
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <footer>
+          <div class="container">
+            <div class="row">
+              <div class="col-md-12"></div>
+                <p>This is Footer !!!</p>
+              </div>
+            </div>
+          </div>
+        </footer>
+    </body>
+</html>
